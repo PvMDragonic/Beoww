21a25fd5e2016f72fc5166235fe2c6757be85d56
PvMDragonic <55903030+PvMDragonic@users.noreply.github.com>
2021-02-22 19:37:33 -0300

    Server v0.3
    -Adição de tratamento para quando um client desconecta.

--- Server/main/ClientHandler.java
+++ Server/main/ClientHandler.java
@@ -12,8 +12,8 @@ public class ClientHandler implements Runnable
 {
     private Socket cliente;
     private Scanner entrada;
-    private DataOutputStream saida;
-    private ArrayList<ClientHandler> clientes;
+    public DataOutputStream saida;
+    public ArrayList<ClientHandler> clientes;
     
     public ClientHandler(Socket clientSocket, ArrayList<ClientHandler> clientes) throws IOException
     {
@@ -21,6 +21,14 @@ public class ClientHandler implements Runnable
         this.clientes = clientes;
         entrada = new Scanner(this.cliente.getInputStream());
         saida = new DataOutputStream(this.cliente.getOutputStream());
+    }
+    
+    private void verificarTodos() throws IOException
+    {
+        for (ClientHandler Cliente : clientes)
+        {
+            Cliente.saida.writeUTF("");
+        }
     }
     
     private void enviarParaTodos(String mensagem) throws IOException
@@ -34,30 +42,32 @@ public class ClientHandler implements Runnable
     @Override
     public void run() 
     {
-        while(this.entrada.hasNextLine())
+        while(entrada.hasNextLine())
         {
+            String mensagem = null;
             try 
             {
-                String mensagem = this.entrada.nextLine();
+                // Vai ler sempre que chegar uma nova mensagem do servidor.
+                mensagem = entrada.nextLine();
                 System.out.println(mensagem.replace("[[[[[", "\n"));
+                
+                verificarTodos();
                 enviarParaTodos(mensagem);
             } 
             catch (IOException ex) 
             {
-                Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                // Vai cair aqui se o 'verificarTodos()' der erro, porque um dos clientes ficou offline.
+                Server.removerDesconectado(mensagem);
+                
+                try 
+                {
+                    enviarParaTodos(mensagem);
+                } 
+                catch (IOException ex1) 
+                {
+                    Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex1);
+                }
             }
         }
-        
-        try 
-        {
-            this.entrada.close();
-            this.saida.close();
-        } 
-        catch (IOException ex) 
-        {
-            Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-    }
-    
+    }   
 }
--- Server/main/Server.java
+++ Server/main/Server.java
@@ -15,22 +15,48 @@ public class Server
     private static ArrayList<ClientHandler> clientes = new ArrayList<>();
     private static ExecutorService pool = Executors.newFixedThreadPool(5);
     
+    public static void removerDesconectado(String mensagem)
+    {
+        // Isso aqui vai rodar até remover todo(s) client(es) faltando. 
+        while (true)
+        {
+            int b = 0;
+            try
+            {
+                for (b = 0; b < clientes.size(); b++)
+                {
+                    clientes.get(b).saida.writeUTF("");
+                }
+                
+                // Se chegar aqui, é porque tá tudo nos trinques, então o 'while (true)' quebra.
+                break;
+            }
+            catch (Exception e)
+            {
+                clientes.remove(b);
+            }
+        }
+    }
+    
     public static void main(String args[])
     {  
         int porta = 25565;
         
         try 
         {
+            // Cria o socket.
             ServerSocket server = new ServerSocket(porta, 0, InetAddress.getByName("192.168.0.3"));
             System.out.println("Servidor iniciado em " + server.getInetAddress().getHostAddress() + ":" + porta);
 
             while (true)
             {
+                // Fica esperando por uma conexão ao socket.
                 Socket cliente = server.accept();
                 System.out.println("Cliente conectado do IP " + cliente.getInetAddress().getHostAddress());
+                
+                // Cria uma nova instância da conexão, e adiciona ela à lista.
                 ClientHandler clientThread = new ClientHandler(cliente, clientes);
                 clientes.add(clientThread);
-
                 pool.execute(clientThread);
             }        
         } 
